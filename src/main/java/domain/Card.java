71cd4cf83d2dd03e12f487fc27c071c4e3aa9c04
Jixin (Kevin) Yan <JixinYan2029@u.northwestern.edu>
2026-05-03 15:05:33 -0500

    feat (card): minimal implementation of first tests to passed

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Card {
+
+	private final CardType cardType;
+
+	public Card(CardType cardType) {
+		this.cardType = cardType;
+	}
+
+	public CardType getCardType() {
+		return cardType;
+	}
+}
