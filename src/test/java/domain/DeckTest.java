948f122beaa5da3daf0c3cfe981114a07dc7a2de
VincentT6472 <vincenttang2029@u.northwestern.edu>
2026-05-06 16:52:51 -0500

    feat (deck): create test for checking deck size after creation

---
+++ src/test/java/domain/DeckTest.java
@@ -0,0 +1,21 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DeckTest {
+
+    @Test
+    public void deck_createFullDeck_correctSize(){
+        Deck deck = new Deck();
+
+        int expectedSize = 56;
+
+        List<Integer> cards = deck.getCards();
+
+        assertEquals(expectedSize, cards.size());
+    }
+}
