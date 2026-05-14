929d47df47303594a89dac04a386618d07ddf820
plentifulplate <h3g5x2@u.northwestern.edu>
2026-05-14 16:05:52 -0500

    NUM_TOTAL_PLAYERS_LESS_THAN_TWO

--- src/main/java/domain/TurnTracker.java
+++ src/main/java/domain/TurnTracker.java
@@ -1,16 +1,22 @@
 package domain;
 
 public class TurnTracker {
-    private int numTotalPlayers = 0;
+    private int numTotalPlayers = 1;
     private int currentPlayer = 0;
     private int currentDirection = 1;
+
+    private static final String NUM_TOTAL_PLAYERS_LESS_THAN_TWO = "turnTracker.numPlayers.tooSmall";
 
     int getNumTotalPlayers() {
         return this.numTotalPlayers;
     }
 
     void setNumTotalPlayers(int numTotalPlayers) {
-        this.numTotalPlayers = numTotalPlayers;
+        if (numTotalPlayers < 2) {
+            throw new IllegalArgumentException(NUM_TOTAL_PLAYERS_LESS_THAN_TWO);
+        } else {
+            this.numTotalPlayers = numTotalPlayers;
+        }
     }
 
     int getCurrentPlayer() {
