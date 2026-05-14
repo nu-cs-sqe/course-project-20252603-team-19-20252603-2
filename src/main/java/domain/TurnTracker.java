99325c64188754b398a6cc5505fc78af521ba954
plentifulplate <h3g5x2@u.northwestern.edu>
2026-05-14 12:12:17 -0500

    turnGoesToNextPlayer_player0_player1 bare production code

--- src/main/java/domain/TurnTracker.java
+++ src/main/java/domain/TurnTracker.java
@@ -1,4 +1,38 @@
 package domain;
 
 public class TurnTracker {
+    private int numTotalPlayers = 0;
+    private int currentPlayer = 0;
+    private int currentDirection = 1;
+
+    int getNumTotalPlayers() {
+        return this.numTotalPlayers;
+    }
+
+    private void setNumTotalPlayers(int numTotalPlayers) {
+        this.numTotalPlayers = numTotalPlayers;
+    }
+
+    int getCurrentPlayer() {
+        return this.currentPlayer;
+    }
+
+    private void setCurrentPlayer(int currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    int getCurrentDirection() {
+        return this.currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public void turnGoesToNextPlayer() {
+        setCurrentPlayer(1);
+    }
+
+
+
 }
