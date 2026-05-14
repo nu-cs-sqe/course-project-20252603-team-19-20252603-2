55aa6b4e848cd3e23b2c234f1ec3f9912526fc1c
plentifulplate <h3g5x2@u.northwestern.edu>
2026-05-14 12:28:24 -0500

    turnGoesToNextPlayer_player2_player0 initial test

--- src/test/java/domain/TurnTrackerTest.java
+++ src/test/java/domain/TurnTrackerTest.java
@@ -10,13 +10,34 @@ public class TurnTrackerTest {
     public void turnGoesToNextPlayer_player0_player1() {
         TurnTracker turnTracker = new TurnTracker();
         final int numTotalPlayers = 3;
+        final int currentPlayer = 0;
+        final int currentDirection = 1;
         turnTracker.setNumTotalPlayers(numTotalPlayers);
-        turnTracker.setCurrentDirection(1);
+        turnTracker.setCurrentPlayer(currentPlayer);
+        turnTracker.setCurrentDirection(currentDirection);
 
         turnTracker.turnGoesToNextPlayer();
 
-        int expectedPlayer = 1;
-        int actualPlayer = turnTracker.getCurrentPlayer();
+        final int expectedPlayer = 1;
+        final int actualPlayer = turnTracker.getCurrentPlayer();
+        assertEquals(expectedPlayer, actualPlayer);
+    }
+
+
+    @Test
+    public void turnGoesToNextPlayer_player2_player0() {
+        TurnTracker turnTracker = new TurnTracker();
+        final int numTotalPlayers = 3;
+        final int currentPlayer = 2;
+        final int currentDirection = 1;
+        turnTracker.setNumTotalPlayers(numTotalPlayers);
+        turnTracker.setCurrentPlayer(currentPlayer);
+        turnTracker.setCurrentDirection(currentDirection);
+
+        turnTracker.turnGoesToNextPlayer();
+
+        final int expectedPlayer = 0;
+        final int actualPlayer = turnTracker.getCurrentPlayer();
         assertEquals(expectedPlayer, actualPlayer);
     }
 
