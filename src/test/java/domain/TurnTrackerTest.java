5f0826a920fb7074426b6f372079e3da1b7a64a1
plentifulplate <h3g5x2@u.northwestern.edu>
2026-05-14 12:04:29 -0500

    turnGoesToNextPlayer_player0_player1 initial test

--- src/test/java/domain/TurnTrackerTest.java
+++ src/test/java/domain/TurnTrackerTest.java
@@ -1,4 +1,19 @@
 package domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TurnTrackerTest {
+
+    @Test
+    public void turnGoesToNextPlayer_player0_player1() {
+        TurnTracker turnTracker = new TurnTracker();
+        turnTracker.turnGoesToNextPlayer();
+
+        int expectedPlayer = 1;
+        int actualPlayer = turnTracker.getCurrentPlayer();
+        assertEquals(expectedPlayer, actualPlayer);
+    }
+
 }
