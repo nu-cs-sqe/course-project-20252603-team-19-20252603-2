7bc60d8c969b763ebe4fdb3537dc02118f638500
plentifulplate <h3g5x2@u.northwestern.edu>
2026-05-14 16:11:39 -0500

    add setCurrentDirection and changeCurrentDirection

--- src/main/java/domain/TurnTracker.java
+++ src/main/java/domain/TurnTracker.java
@@ -31,6 +31,10 @@ public class TurnTracker {
         return this.currentDirection;
     }
 
+    void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
     public void changeCurrentDirection() {
         this.currentDirection *= -1;
     }
