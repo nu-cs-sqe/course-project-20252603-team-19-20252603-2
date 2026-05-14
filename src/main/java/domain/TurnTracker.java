cbc6d48f3ca25456c903689030e9929e1d310702
plentifulplate <h3g5x2@u.northwestern.edu>
2026-05-14 16:35:12 -0500

    turnGoesToCurrentPlayerAgain_2TotalPlayers_shouldRepeatTurnAgain bare production code

--- src/main/java/domain/TurnTracker.java
+++ src/main/java/domain/TurnTracker.java
@@ -1,12 +1,13 @@
 package domain;
 
 public class TurnTracker {
-    private int numTotalPlayers = 1;
+    private int numTotalPlayers = 2;
     private static final int MIN_TOTAL_PLAYERS = 2;
     private int currentPlayer = 0;
     private int currentDirection = 1;
 
     private static final String NUM_TOTAL_PLAYERS_LESS_THAN_TWO = "turnTracker.numPlayers.tooSmall";
+    private static final String INVALID_PLAYING_DIRECTION = "turnTracker.currentDirection.invalid";
 
     public int getNumTotalPlayers() {
         return this.numTotalPlayers;
@@ -15,9 +16,8 @@ public class TurnTracker {
     public void setNumTotalPlayers(int numTotalPlayers) {
         if (numTotalPlayers < MIN_TOTAL_PLAYERS) {
             throw new IllegalArgumentException(NUM_TOTAL_PLAYERS_LESS_THAN_TWO);
-        } else {
-            this.numTotalPlayers = numTotalPlayers;
         }
+        this.numTotalPlayers = numTotalPlayers;
     }
 
     public int getCurrentPlayer() {
@@ -33,6 +33,9 @@ public class TurnTracker {
     }
 
     void setCurrentDirection(int currentDirection) {
+        if (currentDirection != 1 && currentDirection != -1) {
+            throw new IllegalArgumentException(INVALID_PLAYING_DIRECTION);
+        }
         this.currentDirection = currentDirection;
     }
 
@@ -52,6 +55,10 @@ public class TurnTracker {
                 + getCurrentDirection() * 2
                 + getNumTotalPlayers())
                 % getNumTotalPlayers());
+    }
+
+    public void turnGoesToCurrentPlayerAgain() {
+        setCurrentPlayer(getCurrentPlayer());
     }
 
 
