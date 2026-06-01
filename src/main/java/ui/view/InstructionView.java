3632975289febba3e8b79760c38181801c61d030
VincentT6472 <vincenttang2029@u.northwestern.edu>
2026-06-01 16:11:59 -0500

    feat (ui): created the views for each screen

---
+++ src/main/java/ui/view/InstructionView.java
@@ -0,0 +1,6 @@
+package ui.view;
+
+import javafx.scene.layout.StackPane;
+
+public class InstructionView extends StackPane {
+}
---
+++ src/main/java/ui/view/StartView.java
@@ -0,0 +1,7 @@
+package ui.view;
+
+import javafx.scene.layout.StackPane;
+
+public class StartView extends StackPane {
+
+}
