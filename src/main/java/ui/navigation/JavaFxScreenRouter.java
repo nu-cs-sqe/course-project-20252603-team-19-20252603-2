53c5ab0a3048720942b6b6e2906852ef7fb87865
VincentT6472 <vincenttang2029@u.northwestern.edu>
2026-06-01 16:13:07 -0500

    feat (ui): created the router to move between views

---
+++ src/main/java/ui/navigation/JavaFxScreenRouter.java
@@ -0,0 +1,30 @@
+package ui.navigation;
+
+public class JavaFxScreenRouter implements ScreenRouter {
+
+	private ScreenRouter navigation;
+
+	public void configureNavigation(ScreenRouter navigation) {
+		this.navigation = navigation;
+	}
+
+	@Override
+	public void showStart() {
+		navigation.showStart();
+	}
+
+	@Override
+	public void showInstructions() {
+		navigation.showInstructions();
+	}
+
+	@Override
+	public void showGameSetup() {
+		navigation.showGameSetup();
+	}
+
+	@Override
+	public void showGame() {
+		navigation.showGame();
+	}
+}
---
+++ src/main/java/ui/navigation/ScreenRouter.java
@@ -0,0 +1,11 @@
+package ui.navigation;
+
+public interface ScreenRouter {
+	void showStart();
+
+	void showInstructions();
+
+	void showGameSetup();
+
+	void showGame();
+}
