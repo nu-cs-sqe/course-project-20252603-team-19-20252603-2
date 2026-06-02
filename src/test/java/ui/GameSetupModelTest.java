fb3b225878350a00ed11efb9c5f0ed62f64d355d
VincentT6472 <vincenttang2029@u.northwestern.edu>
2026-06-02 18:40:39 -0500

    test (ui): created a test for parsing player name from a list of size 2

--- src/test/java/ui/GameSetupModelTest.java
+++ src/test/java/ui/GameSetupModelTest.java
@@ -22,4 +22,13 @@ class GameSetupModelTest {
 						List.of("Vincent"), DEFAULT_PREFIX));
 		assertEquals(TOO_FEW_PLAYERS_KEY, ex.getMessage());
 	}
+
+	@Test
+	void capturePlayerNamesFromInputs_listSizeTwo_storesBothNames() {
+		GameSetupModel model = new GameSetupModel();
+
+		model.capturePlayerNamesFromInputs(
+				List.of("Vincent", "Vinny"), DEFAULT_PREFIX);
+		assertEquals(List.of("Vincent", "Vinny"), model.getPlayerNames());
+	}
 }
