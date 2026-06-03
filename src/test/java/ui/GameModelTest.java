65281a966f7852e6ba65bf3a6f555dc3db34c6ea
VincentT6472 <vincenttang2029@u.northwestern.edu>
2026-06-03 01:16:28 -0500

    test (ui): created a test for starting game with 2 players and first player is correct

--- src/test/java/ui/GameModelTest.java
+++ src/test/java/ui/GameModelTest.java
@@ -1,5 +1,6 @@
 package ui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -13,5 +14,15 @@ class GameModelTest {
         GameModel model = new GameModel();
         model.startGame(List.of("P0", "P1"));
         assertTrue(model.isGameStarted());
+    }
+
+    @Test
+    void startGame_twoPlayers_firstPlayerP0() {
+        GameModel model = new GameModel();
+        model.startGame(List.of("P0", "P1"));
+
+        String expectedLocalPlayer = "P0";
+
+        assertEquals(expectedLocalPlayer, model.getLocalPlayerName());
     }
 }
