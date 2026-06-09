365f91c37c5318283c9284edc485bbcc54233e25
VincentT6472 <vincenttang2029@u.northwestern.edu>
2026-06-09 17:08:55 -0500

    test (rule): created a test for playing the cat triple with 2 cat cards and 1 clone

--- src/test/java/domain/RuleManagerTest.java
+++ src/test/java/domain/RuleManagerTest.java
@@ -224,6 +224,15 @@ class RuleManagerTest {
     }
 
     @Test
+    void requireCatTriple_twoCatCardAndOneClone_returnsNormally() {
+        Player actor = new Player(0);
+        actor.addCardToHand(new Card(CardType.CAT_CARDS));
+        actor.addCardToHand(new Card(CardType.CAT_CARDS));
+        actor.addCardToHand(new Card(CardType.CLONE));
+        assertDoesNotThrow(() -> ruleManager.requireCatTriple(actor, CardType.CAT_CARDS));
+    }
+
+    @Test
     void requireSomethingToNope_nonNullLastCard_returnsNormally() {
         assertDoesNotThrow(() -> ruleManager.requireSomethingToNope(CardType.ATTACK));
     }
