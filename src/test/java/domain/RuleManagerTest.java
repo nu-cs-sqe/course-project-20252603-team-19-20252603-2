19de29d7d49311646d5df081b602603724bcfb75
VincentT6472 <vincenttang2029@u.northwestern.edu>
2026-06-09 00:24:31 -0500

    test (game): created a test for testing catPair with cat card and feral cat card

--- src/test/java/domain/RuleManagerTest.java
+++ src/test/java/domain/RuleManagerTest.java
@@ -114,6 +114,14 @@ class RuleManagerTest {
     }
 
     @Test
+    void requireCatPair_oneCatCardAndOneFeralCatCard_returnsNormally() {
+        Player actor = new Player(0);
+        actor.addCardToHand(new Card(CardType.CAT_CARDS));
+        actor.addCardToHand(new Card(CardType.FERAL_CAT));
+        assertDoesNotThrow(() -> ruleManager.requireCatPair(actor, CardType.CAT_CARDS));
+    }
+
+    @Test
     void requireCatTriple_threeOfType_returnsNormally() {
         Player actor = new Player(0);
         actor.addCardToHand(new Card(CardType.CAT_CARDS));
